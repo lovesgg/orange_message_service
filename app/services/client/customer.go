0caf33fc978c0c983aacc753c13de8ef6ef45a23
suguangguo <suguangguo@meicai.cn>
2020-04-20 14:19:23 +0800

    优化自动回复的字符匹配 支持多条对话数据返回

--- app/services/client/customer.go
+++ app/services/client/customer.go
@@ -2,37 +2,47 @@ package services
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/kataras/iris/context"
 	"orange_message_service/app/components/acm"
 	models "orange_message_service/app/models/request"
+	"strings"
 )
 
 /**
 acm配置信息 举例
-
 {
-    "test":{
-        "text":"你好"
-    },
-	"order":{
-		"text":"订单不存在"
-    }
+    "words": [
+        {
+            "title":"发货",
+            "detail":"20日之后发货"
+        },
+        {
+            "title":"物流哈哈哈",
+            "detail":"20日之后发运 顺丰"
+        }
+    ]
 }
 */
 
-func CustomerSay(ctx context.Context, req models.CustomerSayReq) string {
-	var words map[string]map[string]string
+func CustomerSay(ctx context.Context, req models.CustomerSayReq) []map[string]string {
+	var words map[string][]map[string]string
+	var returnData []map[string]string
 
 	getConfig := acm.GetConfig("test", "DEFAULT_GROUP")
 	_ = json.Unmarshal([]byte(getConfig), &words)
-	data, ok := words[req.Text]
-	fmt.Println(data, ok)
+	data := words["words"]
 
-	if ok {
-		return data["text"] //取的是配置里的自动回复内容
-	} else {
-		fmt.Println("##########")
-		return "暂时没有回答"
+	for _, value := range data {
+		//匹配子字符串 有的话数组返回
+		index := strings.Index(value["title"], req.Text)
+		if index >= 0 {
+			word := map[string]string{
+				"title":  value["title"],
+				"detail": value["detail"],
+			}
+			returnData = append(returnData, word)
+		}
 	}
+
+	return returnData
 }
